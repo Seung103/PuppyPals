9ff03a4998575e41abcfba6705b6f5918ce17b44
Seung-Woo Seo <seungwoo103@gmail.com>
2024-09-23 17:36:30 -0400

    implemented map statement on React, making it show on html as a list(array)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
+
+export default function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
+}
