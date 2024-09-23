0264f2930ed8179191a26729970b3866c7611e8d
Seung-Woo Seo <seungwoo103@gmail.com>
2024-09-23 19:17:25 -0400

    finished working on the assignment, Morgen helped me understand fundamental funciontality for map on JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import { useState } from "react";
 import { puppyList } from "./data.js";
+import "./App.css";
 
 export default function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    () => {
+      console.log("puppy id: ", puppy.id);
+    };
+  }
+
   console.log(puppies);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <div className="App">
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              console.log("puppy id: ", puppy.id);
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
